a2e40aa9505919a7e03474828049e76e4dfefc23
allysonlee <allysonlee@berkeley.edu>
2026-07-09 14:09:05 -0700

    Week 2 deliverable fixed

--- week2/week2.py
+++ week2/week2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-sold = pd.read_csv("C:/Users/izlal/IDXExchange_SU26/week1/ConcatenatedCRMLSSold.csv")
+sold = pd.read_csv("C:/Users/izlal/IDXExchange_SU26/week2/CRMLSSoldAll.csv")
 
 # Identify numbers of rows and columns
 sold_rows = len(sold)
@@ -37,46 +37,46 @@ print("Flagged missing columns: ", missing_sum.index[missing_sum["Flag"]])
 # Produce a numeric distribution summary (min, max, mean, median, percentiles) for ClosePrice, LivingArea, and DaysOnMarket
 target_cols = ["ClosePrice", "LivingArea", "DaysOnMarket"]
 for col in target_cols:
-    print(sold_filtered[col].describe())
+    print(sold_filtered[col].describe().apply(lambda x: f"{x:.2f}"))
 
 # Save the filtered dataset as a new CSV
 sold_filtered.to_csv("CRMLSSoldFiltered.csv")
 
 # January 2024 to May 2026 Sold Data Analysis
 ###
-# Sold Number of Rows:  430445
+# Sold Number of Rows:  639916
 # Sold Number of Columns:  83
 # Sold Column Datatypes:  ['int64', 'str', 'object', 'float64']
-# Sold Property Types: 'BusinessOpportunity', 'CommercialLease', 'CommercialSale', 'Land', 'ManufacturedInPark', 'ResidentialIncome', 'ResidentialLease')
-# Flagged Missing Columns: 'WaterfrontYN', 'BasementYN', 'FireplacesTotal', 'AboveGradeFinishedArea', 'TaxAnnualAmount', 'BuilderName', 'TaxYear', 'BuildingAreaTotal', 'ElementarySchoolDistrict', 'CoBuyerAgentFirstName', 'BelowGradeFinishedArea', 'BusinessType', 'CoveredSpaces', 'LotSizeDimensions', 'MiddleOrJuniorSchoolDistrict'
+# Sold Property Types: ['Residential', 'CommercialLease', 'Land', 'ResidentialLease', 'ManufacturedInPark', 'ResidentialIncome', 'CommercialSale', 'BusinessOpportunity']
+# Flagged Missing Columns: ['WaterfrontYN', 'BasementYN', 'FireplacesTotal', 'AboveGradeFinishedArea', 'TaxAnnualAmount', 'BuilderName', 'TaxYear', 'BuildingAreaTotal', 'ElementarySchoolDistrict', 'CoBuyerAgentFirstName', 'BelowGradeFinishedArea', 'BusinessType', 'CoveredSpaces', 'LotSizeDimensions', 'MiddleOrJuniorSchoolDistrict']
 ###
 # Numeric Distribution Summary: 
 # ClosePrice:
-# count    4.304430e+05
-# mean     1.193117e+06
-# std      6.174121e+06
-# min      0.000000e+00
-# 25%      5.750000e+05
-# 50%      8.250000e+05
-# 75%      1.300000e+06
-# max      9.895000e+08
+# count       430443.00
+# mean       1193116.77
+# std        6174121.01
+# min              0.00
+# 25%         575000.00
+# 50%         825000.00
+# 75%        1300000.00
+# max      989500000.00
 ##
 # LivingArea: 
-# count    4.302000e+05
-# mean     1.904068e+03
-# std      2.596802e+04
-# min      0.000000e+00
-# 25%      1.248000e+03
-# 50%      1.644000e+03
-# 75%      2.221000e+03
-# max      1.702132e+07
+# count      430200.00
+# mean         1904.07
+# std         25968.02
+# min             0.00
+# 25%          1248.00
+# 50%          1644.00
+# 75%          2221.00
+# max      17021321.00
 ##
 # DaysOnMarket:
-# count    430445.000000
-# mean         37.333627
-# std          53.668097
-# min        -288.000000
-# 25%           8.000000
-# 50%          18.000000
-# 75%          48.000000
-# max       12430.000000
+# count    430445.00
+# mean         37.33
+# std          53.67
+# min        -288.00
+# 25%           8.00
+# 50%          18.00
+# 75%          48.00
+# max       12430.00
